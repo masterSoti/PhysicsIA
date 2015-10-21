9f301eaa6f5fb9e951eb760dcb72fdddd3065e50
Suyog Soti <ssoti2201@bvsd.org>
2015-10-21 11:24:14 -0600

    minor corrections

--- FIndMax.py
+++ FIndMax.py
@@ -1,16 +1,18 @@
 from openpyxl import load_workbook
 
+print "THis script runs"
 wb = load_workbook(filename="Spring.xlsx")
 sheet_ranges = wb['Sheet1']
-f = open('max.txt', 'w')
-collum = raw_input("Please enter the Collum of the max that you want to find...\n")
+f = open('max_run_four.txt', 'w')
 for i in range(3, 20000):
-	collum = str(collum)
+	collum = 'K'
+	xcollum = 'J'
 	var1 = collum+str(i)
 	var2 = collum+str(i+1)
+	varx = xcollum+str(i)
 	first = sheet_ranges[var1].value
 	second = sheet_ranges[var2].value
 	if(first > second):
-		s = str(sheet_ranges[var1].value)+",\n"
+		s = str(sheet_ranges[varx].value) + ",\t" + str(sheet_ranges[var1].value)+",\n"
 		f.write(s)
 f.close()
