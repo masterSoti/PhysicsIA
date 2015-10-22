4317768b6cd38803a0a01878585f42bb6c60aed0
Suyog Soti <ssoti2201@bvsd.org>
2015-10-21 21:56:33 -0600

    made it so that it also graphs

--- FIndMax.py
+++ FIndMax.py
@@ -1,18 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from openpyxl import load_workbook
 
 print "THis script runs"
 wb = load_workbook(filename="Spring.xlsx")
 sheet_ranges = wb['Sheet1']
 f = open('max_run_four.txt', 'w')
+xdata = []
+ydata = []
 for i in range(3, 20000):
 	collum = 'K'
 	xcollum = 'J'
 	var1 = collum+str(i)
 	var2 = collum+str(i+1)
 	varx = xcollum+str(i)
+	var0 = collum+str(i-1)
 	first = sheet_ranges[var1].value
+	xfirst = sheet_ranges[varx].value
 	second = sheet_ranges[var2].value
-	if(first > second):
-		s = str(sheet_ranges[varx].value) + ",\t" + str(sheet_ranges[var1].value)+",\n"
-		f.write(s)
+	last = sheet_ranges[var0].value
+	if((first < second) & (first > last)):
+		xdata.append(xfirst)
+		ydata.append(first)
+		#s = str(sheet_ranges[varx].value) + ",\t" + str(sheet_ranges[var1].value)+",\n"
+		#f.write(s)
+#print xdata
+#print ydata
 f.close()
+plt.plot(xdata, ydata, 'k.')
+plt.show()
